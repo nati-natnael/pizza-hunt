c026c12dcc367c248f136d0275ade231f7b41185
unknown <natnaelger@gmail.com>
2022-04-19 18:51:36 -0700

    initial commit

---
+++ models/Pizza.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose')
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: [],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
+},
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false
+})
+
+const Pizza = model('Pizza', PizzaSchema)
+
+module.exports = Pizza
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.set('debug', true)
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
